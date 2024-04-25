fe52aada337809377e6da01e492ce2dd715c2f28
Driggsz <rodrigogx2009@hotmail.com>
2024-04-25 18:01:19 -0300

    Final

--- src/sections/NavBar/index.js
+++ src/sections/NavBar/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Tilt from "react-tilt";
 import Logo from "../../assets/logoipsum.png";
 import "./styles.css";
@@ -8,40 +8,30 @@ import Roll from "react-reveal/Roll";
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
-const toggleMenu = () => {
-  setIsOpen(!isOpen);
-};
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="navbar">
       <div className="container-links">
-    
-      <div className="logo">
-        <a href="/" target="_blank">
-          <img src={Logo} className="logo-img" alt="Logo" />
-        </a>
-      </div>
+        <div className="logo">
+          <a href="/" target="_blank">
+            <img src={Logo} className="logo-img" alt="Logo" />
+          </a>
+        </div>
 
-      <div className="links">
- 
+        <div className="links">
           <a className="link" href="">
             Sobre
           </a>
-
           <a className="link" href="">
             Portfolio
           </a>
-      
-
- 
           <a className="link" href="">
             Preços
           </a>
-
-     
-    
-        
-      </div>
-
+        </div>
       </div>
 
       <Tilt
@@ -49,35 +39,34 @@ const toggleMenu = () => {
         options={{ max: 25 }}
         style={{ height: 50, width: 250 }}
       >
-      <div className="botao">
-        <a className="contato" href="https://api.whatsapp.com/send?phone=5541996745640&text=Ol%C3%A1!%20vim%20do%20Site%20da%20Cardial%20;)">
-          Marcar uma call
-        </a>
-      </div>
+        <div className="botao">
+          <a
+            className="contato"
+            href="https://api.whatsapp.com/send?phone=5541996745640&text=Ol%C3%A1!%20vim%20do%20Site%20da%20Cardial%20;)"
+          >
+            Marcar uma call
+          </a>
+        </div>
       </Tilt>
 
-{/* 
-      <a  className="button-menu-hamburguer" onClick={toggleMenu}>
-  <GiHamburgerMenu className="hamburguer-icon" />
-</a> */}
+      <a className="button-menu-hamburguer" onClick={toggleMenu}>
+        <GiHamburgerMenu className="hamburguer-icon" />
+      </a>
 
-<Roll right>
-<div className={`popup-menu ${isOpen ? "open" : ""}`}>
-  <div className="popup-menu-content">
-  <button className="close-btn" onClick={toggleMenu}>
-    X
-  </button>
-  <div className="links-responsive">
-    <a href="#sobre">Sobre</a>
-    <a href="#projetos">Produtos</a>
-    <a href="#experiencia">Equipe</a>
-    </div>
-  </div>
-</div>
-</Roll>
-
-
-
+      <Roll right>
+        <div className={`popup-menu ${isOpen ? "open" : ""}`}>
+          <div className="popup-menu-content">
+            <button className="close-btn" onClick={toggleMenu}>
+              X
+            </button>
+            <div className="links-responsive">
+              <a href="#sobre">Sobre</a>
+              <a href="#projetos">Produtos</a>
+              <a href="#experiencia">Equipe</a>
+            </div>
+          </div>
+        </div>
+      </Roll>
     </header>
   );
 }
